f6076de3bf607006e1c5d60f12f9c1868f0d5b23
carlos <carlos.molina.munoz@gmail.com>
2018-06-13 01:25:55 -0400

    segundo commit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,52 @@
 class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+  end
+
+  def index
+    @todo = Todo.new
+
+  end
+
+  def create
+    @todo = Todo.new(post_params)
+    @todo.save
+    redirect_to todos_path
+
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update(post_params)
+    redirect_to todos_path
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    redirect_to todos_path
+
+  end
+
+  def complete
+    @todo = Todo.find(params[:id])
+    @todo.completed = true
+    @todo.save
+    redirect_to todos_path
+
+  end
+
+  private
+  def post_params
+    params.require(:todo).permit(:description, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'todos', to: 'todos#index'
+  get 'todos/new'
+  post 'todos', to: 'todos#create'
+  get 'todos/:id', to: 'todos#show', as: 'post'
+  patch 'todos/:id/edit', to: 'todos#edit'
+  patch 'todos/:id', to: 'todos#update'
+  delete 'todos/:id', to: 'todos#destroy'
+  get 'todos/:id', to: 'todos#complete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
